2bc35b54a4f2b28d27c4cf3948aedaa26969b440
DAISUKE IZUTSU <idutsudaisuke@gmail.com>
2021-03-17 06:53:27 +0900

    md

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 add_action( 'wp_enqueue_scripts', function(){
     wp_enqueue_style( 'slick-style', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array('theme-style'));
-    wp_enqueue_style( 'slider-style', THEME_URL.'/mmm-slider/style.css', array('slick-style'), filemtime( dirname(__FILE__).'/style.css') );
+    wp_enqueue_style( 'slider-style', THEME_URL.'/libs/mmm-slider/style.css', array('slick-style'), filemtime( dirname(__FILE__).'/style.css') );
 	wp_enqueue_script( 'slick-js','//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), false );
 });
 
@@ -14,9 +14,9 @@ add_action( 'wp_enqueue_scripts', function(){
 @param string $id
 @param array $images
 @example：
-    $id = '#slideshow-1;
+    $id = "slideshow-1";
     $images = array(
-        array( 'src' => THEME_URL.'/images/slide1.jpg', 'alt' => 'スライド１'  ),
+        array( 'src' => THEME_URL.'/images/slide1.jpg', 'alt' => 'スライド１', 'link' => 'https://'  ),
         array( 'src' => THEME_URL.'/images/slide2.jpg', 'alt' => 'スライド２'  ),
         array( 'src' => THEME_URL.'/images/slide3.jpg', 'alt' => 'スライド３' )
     );
@@ -27,20 +27,20 @@ add_action( 'wp_enqueue_scripts', function(){
 function mmm_slider( $id, $images ){
     ob_start();
     ?>
-    <div id="<?php echo $id; ?>" class="mmm-slider">
+    <ul id="<?php echo $id; ?>" class="mmm-slider">
         <?php
             $count = 1;
             foreach( $images as $image ){
                 ?>
-                    <img class="mmm-slider__img" src="<?php echo $image['src'] ? $image['src'] : ''; ?>" alt="<?php echo $image['alt'] ? $image['alt'] : '画像'.$count; ?>" />
+                   <li><?php if($image['link']){ ?><a href="<?php echo $image['link']; ?>"><?php } ?><img class="mmm-slider__img" src="<?php echo $image['src'] ? $image['src'] : ''; ?>" alt="<?php echo $image['alt'] ? $image['alt'] : '画像'.$count; ?>" /><?php if($image['link']){ ?></a><?php } ?></li>
                 <?php
                 $count ++;
             }
         ?>
-    </div>
+    </ul>
     <script>
         (function($){
-            $("<?php echo $id; ?>").slick({
+            $("#<?php echo $id; ?>").slick({
                 autoplay:true,
                 autoplaySpeed:5000,
                 arrows:true,
